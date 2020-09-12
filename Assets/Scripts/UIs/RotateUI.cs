0060ac037bdb0b340ce67a05b2531a3ad64d959b
Kleber de Oliveira Andrade <kleber.andrade@playbrandsgroup.com>
2020-09-12 13:11:16 -0300

    Aula 4

---
+++ Assets/Scripts/UIs/RotateUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotateUI : MonoBehaviour
+{
+    public Vector3 m_Axis = Vector3.forward;
+    public float m_Speed = 180.0f;
+
+    public void Update()
+    {
+        transform.Rotate(m_Axis * m_Speed * Time.deltaTime);
+    }
+}

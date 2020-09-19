aa5992164657f18e6c09293884e5f4036d0ce603
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2020-09-19 13:02:15 -0300

    Aula 5

---
+++ Assets/Scripts/Pause/Pause.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Pause : MonoBehaviour
+{
+    public bool m_IsPaused { get; set; }
+
+    public GameObject m_PausePanel;
+
+    public void Show()
+    {
+        m_IsPaused = true;
+        m_PausePanel.SetActive(m_IsPaused);
+        Time.timeScale = 0.0f;
+    }
+
+    public void Hide()
+    {
+        m_PausePanel.SetActive(false);
+        m_IsPaused = false;
+        Time.timeScale = 1.0f;
+    }
+
+    public void Toggle()
+    {
+        if (m_IsPaused)
+            Hide();
+        else 
+            Show();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) Toggle();
+    }
+}

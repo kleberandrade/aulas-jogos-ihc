5408611a2be21f510232b642b54344e0c7aaaff3
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2020-10-03 13:07:10 -0300

    Aula de minimap

---
+++ Assets/Scripts/Minimap/MinimapCameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MinimapCameraFollow : MonoBehaviour
+{
+    public Transform m_Target;
+    public float m_Height = 20.0f;
+
+    private void LateUpdate()
+    {
+        if (!m_Target) return;
+
+        Vector3 position = m_Target.position;
+        position.y += m_Height;
+
+        transform.position = position;
+    }
+}
